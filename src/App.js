a71bad9e9fbde6f661a45f9463e711ad6d37fab8
Alejandro-Naranjo <heineralejo@gmail.com>
2022-05-16 15:52:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/Navbar/NavBar'
+
+
+
+let styleh1 = {
+  color : 'red',
+  border : '10px',
+  marginbottom : '20px'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="saludo" style={styleh1}>
+      <NavBar />
+    <h1>Hola Narflex</h1>
     </div>
   );
 }
---
+++ src/Components/Navbar/producto.js
@@ -0,0 +1,11 @@
+//productos cards
+
+const Producto = () => {
+    return (
+        <div>
+            <img src="../descarga.jpg"/>
+        </div>
+    )
+}
+
+export default Producto
