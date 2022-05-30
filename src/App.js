487a02953dae00887042fdcdb7a7ce21f995c322
Alejandro-Naranjo <heineralejo@gmail.com>
2022-05-30 16:14:50 -0300

    clase 6 detalles

--- src/App.js
+++ src/App.js
@@ -2,50 +2,12 @@ import './App.css';
 import NavBar from './Components/Componentes/NavBar/NavBar'
 import ItemListContainer from './Components/Componentes/Producto/ItemListContainer';
 import { useState , useEffect } from 'react';
-import Modal from './Components/Componentes/Modal/Modal';
-import Producto from './Components/Componentes/Producto/Producto';
+import productos from './Components/Componentes/Producto/Productos';
+import ContenedorProductos from './Components/Componentes/Producto/ContenedorProductos';
 
 function App() {
   const [open, setOpen] = useState(false)
-
-  const handleClose = () => {
-    setOpen(false)
-  }
   const [products, setProducts] = useState([])
-  const productos = [
-    {
-        title : 'Colchon D30 200*190',
-        price : 50000,
-        image : './imagenes/ColchonD30-200.jpeg',
-        description : '',
-        stock : 2,
-        id : 1
-    },
-    {
-        title : 'Colchon D30 140*190',
-        price : 45000,
-        image : './imagenes/ColchonD30-140.jpeg',
-        description : '',
-        stock : 3,
-        id : 2
-    },
-    {
-        title : 'Colchon D30 120*190',
-        price : 40000,
-        image : './imagenes/ColchonD30-120.jpeg',
-        description : '',
-        stock : 5,
-        id : 3
-    },
-    {
-        title : 'Colchon D30 100*190',
-        price : 30000,
-        image : './imagenes/ColchonD30-100.jpeg',
-        description : '',
-        stock : 10,
-        id : 4
-    },
-  ]
 
   const getProducts = () => {
     return new Promise( (resolve, reject) => {
@@ -55,17 +17,16 @@ function App() {
     })
 }  
 
-useEffect( () => {
-    getProducts()
-    .then( (response) => {
-        setProducts(response)
-    })
-    .catch( (err) => {
-    })
-    .finally( () => {
-    })
-}, [])
-
+  useEffect( () => {
+      getProducts()
+      .then( (response) => {
+          setProducts(response)
+      })
+      .catch( (err) => {
+      })
+      .finally( () => {
+      })
+  }, [])
 
   return (
     <div className="App">
@@ -73,9 +34,27 @@ useEffect( () => {
       <div className='Producto-Item'>
         <ItemListContainer title={'Productos D30'} products={productos}/>
       </div>
-      <button onClick={() => setOpen(true)}>Abrir modal</button>
+        <button onClick={() => setOpen(true)}>Abrir modal</button>
+      <div>
+        <ContenedorProductos/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+/* llamada APi  */
+/* useEffect(() =>{
+  fetch()
+  .then((response) => {
+    response.json()
+  })
+  .then((data) => {
+    response.log("Respuesta : ", data)
+  })
+}, []) */
---
+++ src/Components/Componentes/Producto/Productos.js
@@ -0,0 +1,45 @@
+const productos = [
+    {
+        title : 'Colchon D30 200*190',
+        price : 50000,
+        image : './imagenes/ColchonD30-200.jpeg',
+        description : '',
+        stock : 2,
+        id : 1
+    },
+    {
+        title : 'Colchon D30 140*190',
+        price : 45000,
+        image : './imagenes/ColchonD30-140.jpeg',
+        description : '',
+        stock : 3,
+        id : 2
+    },
+    {
+        title : 'Colchon D30 120*190',
+        price : 40000,
+        image : './imagenes/ColchonD30-120.jpeg',
+        description : '',
+        stock : 5,
+        id : 3
+    },
+    {
+        title : 'Colchon D30 100*190',
+        price : 30000,
+        image : './imagenes/ColchonD30-100.jpeg',
+        description : '',
+        stock : 10,
+        id : 4
+    },
+]
+const producto1 = {
+        title : 'Colchon D30 200*190',
+        price : 50000,
+        image : './imagenes/ColchonD30-200.jpeg',
+        description : '',
+        stock : 2,
+        id : 1
+}
+
+export default productos
+export {producto1}
